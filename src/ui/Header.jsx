a9c9b5edee89a22e3537c1f5083f369b27b68ad8
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-01 00:46:52 +0600

    building the app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Wanderer's Pizza</Link>
+
+      <p>Abeer</p>
+    </header>
+  );
+}
+
+export default Header;
