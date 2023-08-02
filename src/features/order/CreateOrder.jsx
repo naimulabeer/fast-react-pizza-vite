830dee3860cc8c2d90d833553e935ef7567f1b46
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-03 00:47:08 +0600

    reusing styles with @ apply

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -49,13 +49,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -63,13 +63,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className="border-stone-200 rounded-full border focus:ring focus:ring-green-400
-              px-4 py-2 md:px-6 md:py-3 transition-all duration-300"
-            />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
