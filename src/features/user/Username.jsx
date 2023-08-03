f10d287b459588e451444beef9e4423f256c9a5d
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-03 18:16:49 +0600

    modeling userstate with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden  text-sm font-semibold">Abeer</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+  return (
+    <div className="hidden md:block text-sm font-semibold">{username}</div>
+  );
 }
 
 export default Username;
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,3 +1,4 @@
+/*
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -19,3 +20,24 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
